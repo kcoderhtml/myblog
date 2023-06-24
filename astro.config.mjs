1f0222d528e96188e3734d0d4902f7c446af3d5d
Kieran Klukas - kierank@Glaedrs-Nest.local <92754843+kcoderhtml@users.noreply.github.com>
2023-06-24 17:17:25 -0400

    :recycle: Refactor code and use Astro Netlify adapter
    This commit refactors the code to use the Astro Netlify adapter for deployment, updating both `astro.config.mjs` and `.gitignore` files accordingly. Additionally, it uses consistent import formatting for all imported modules, and imports `netlify` from `@astrojs/netlify/functions`.
    
    It also updates the `tsconfig.json` file to use `strictNullChecks` and include the necessary types from `@vrite/sdk/types` and `astro/client`.
    
    Finally, it refactors the `index.astro` file to use consistent import formatting and removes the trailing whitespace in the file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,25 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import { vritePlugin } from '@vrite/sdk/astro';
-import sitemap from '@astrojs/sitemap';
-import { loadEnv } from 'vite';
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify/functions";
+import mdx from "@astrojs/mdx";
+import { vritePlugin } from "@vrite/sdk/astro";
+import sitemap from "@astrojs/sitemap";
+import { loadEnv } from "vite";
 
 const { VRITE_ACCESS_TOKEN, VRITE_CONTENT_GROUP_ID } = loadEnv(
-  import.meta.env.MODE,
-  process.cwd(),
-  ''
+	import.meta.env.MODE,
+	process.cwd(),
+	""
 );
 
 export default defineConfig({
-  site: 'https://kieranklukas.com',
-  integrations: [
-    mdx(),
-    sitemap(),
-    vritePlugin({
-      accessToken: VRITE_ACCESS_TOKEN,
-      contentGroupId: VRITE_CONTENT_GROUP_ID,
-    }),
-  ],
+	adapter: netlify(),
+	site: "https://kieranklukas.com",
+	integrations: [
+		mdx(),
+		sitemap(),
+		vritePlugin({
+			accessToken: VRITE_ACCESS_TOKEN,
+			contentGroupId: VRITE_CONTENT_GROUP_ID,
+		}),
+	],
 });
