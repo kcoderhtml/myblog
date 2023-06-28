5818eff52e91fdf7cea333636b4091bd8259ee74
Kieran Klukas - kierank@Glaedrs-Nest.local <92754843+kcoderhtml@users.noreply.github.com>
2023-06-28 11:30:36 -0400

    feat: Refactor Nav component and update portfolio page
    - Refactored Nav component to fix selected class logic for "blog" link
    - Updated portfolio page to import getPortfolioItems function from portfolioItems module
    
    :recycle: :art:

--- src/utils/portfolioItems.ts
+++ src/utils/portfolioItems.ts
@@ -1,17 +1,75 @@
 import { createClient } from "@vrite/sdk/api";
+import type { Client } from "@vrite/sdk/api";
+import type { ContentPieceWithAdditionalData } from "@vrite/sdk/api";
+
 
 const client = createClient({
     token: import.meta.env.VRITE_ACCESS_TOKEN
 });
 
-export module PortfolioItems {
-    export const getPortfolioItems = async () => {
-        const contentPieces = await client.contentPieces.list({
-            contentGroupId: import.meta.env.VRITE_PORTFOLIO_GROUP_ID,
+const getContentPieces = async (
+    contentGroupId: string,
+    config?: {
+        limit?: number | "all";
+        startPage?: number;
+        tagId?: string;
+    }
+): Promise<Array<Omit<ContentPieceWithAdditionalData, "content">>> => {
+    const contentPieces: Array<Omit<ContentPieceWithAdditionalData, "content">> = [];
+
+    let page = config?.startPage || 1;
+
+    const fetchPage = async (): Promise<void> => {
+        const paginatedContentPieces = await (client as Client).contentPieces.list({
+            contentGroupId,
+            page,
+            perPage: config?.limit === "all" ? 50 : config?.limit || 50,
+            tagId: config?.tagId
         });
 
-        return {
-            contentPieces: contentPieces
-        };
-    }
+        contentPieces.push(...paginatedContentPieces);
+
+        if (config?.limit === "all" && paginatedContentPieces.length === 50) {
+            page += 1;
+            await fetchPage();
+        }
+    };
+
+    await fetchPage();
+
+    return contentPieces;
 };
+
+const getPortfolioItems = async () => {
+    const contentPieces = await client.contentPieces.list({
+        contentGroupId: import.meta.env.VRITE_PORTFOLIO_GROUP_ID,
+    });
+
+    return {
+        contentPieces: contentPieces
+    };
+}
+
+const getStaticPaths = async (
+    contentGroupId: string
+): Promise<
+    Array<{
+        params: { slug: string };
+        props: Omit<ContentPieceWithAdditionalData, "content">;
+    }>
+> => {
+    const contentPieces = await getContentPieces(import.meta.env.VRITE_PORTFOLIO_GROUP_ID, {
+        limit: "all"
+    });
+
+    return contentPieces.map((contentPiece) => {
+        return {
+            params: {
+                slug: contentPiece.slug
+            },
+            props: contentPiece
+        };
+    });
+};
+
+export { getContentPieces, getPortfolioItems, getStaticPaths }
